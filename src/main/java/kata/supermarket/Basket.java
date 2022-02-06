29e764f9ff9bc02f76d86f745db8ae850ef9c3f4
gsmit592 <gsmit592@ford.com>
2022-02-06 13:57:13 +0000

    Passing in the items to the calculator so no longer need to initialize TotalCalculator with the basket/items

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -4,27 +4,20 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.unmodifiableList;
-
 public class Basket {
     private final List<Item> items;
     private final TotalCalculator totalCalculator;
 
     public Basket() {
         this.items = new ArrayList<>();
-        totalCalculator = new TotalCalculator(this);
+        totalCalculator = new TotalCalculator();
     }
 
     public void add(final Item item) {
         this.items.add(item);
     }
 
-    List<Item> items() {
-        return unmodifiableList(items);
-    }
-
     public BigDecimal total() {
-        return totalCalculator.calculate();
+        return totalCalculator.calculate(items);
     }
-
 }
--- src/main/java/kata/supermarket/TotalCalculator.java
+++ src/main/java/kata/supermarket/TotalCalculator.java
@@ -5,19 +5,12 @@ import java.math.RoundingMode;
 import java.util.List;
 
 class TotalCalculator {
-    private final Basket basket;
-    private final List<Item> items;
 
-    TotalCalculator(Basket basket) {
-        this.basket = basket;
-        this.items = basket.items();
+    public BigDecimal calculate(List<Item> items) {
+        return subtotal(items).subtract(discounts());
     }
 
-    public BigDecimal calculate() {
-        return subtotal().subtract(discounts());
-    }
-
-    private BigDecimal subtotal() {
+    private BigDecimal subtotal(List<Item> items) {
         return items.stream().map(Item::price)
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO)
