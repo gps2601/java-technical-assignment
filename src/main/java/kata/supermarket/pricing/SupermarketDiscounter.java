d4d21511c49e11c838fdb7edeca97ff9be51e01c
gsmit592 <gsmit592@ford.com>
2022-02-06 14:31:59 +0000

    SupermarketDiscounter now handles applying discounts to different products

--- src/main/java/kata/supermarket/pricing/SupermarketDiscounter.java
+++ src/main/java/kata/supermarket/pricing/SupermarketDiscounter.java
@@ -4,17 +4,41 @@ import kata.supermarket.product.Item;
 import kata.supermarket.product.ProductName;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static java.math.BigDecimal.ZERO;
+import static java.util.stream.Collectors.groupingBy;
 
 public class SupermarketDiscounter implements Discounter {
+    private final Map<ProductName, Discount> discountedProducts = new HashMap<>();
+
     @Override
     public BigDecimal calculateDiscounts(List<Item> items) {
-        return ZERO;
+        Map<ProductName, List<Item>> itemsByDiscount = groupItemsByDiscountEligibility(items);
+        return itemsByDiscount
+                .entrySet()
+                .stream()
+                .map(productItems -> {
+                    List<Item> discountableItems = productItems.getValue();
+                    Discount discount = discountedProducts.get(productItems.getKey());
+                    return discount.discountFor(discountableItems);
+                })
+                .reduce(BigDecimal::add)
+                .orElse(ZERO);
     }
 
     public void addDiscount(ProductName name, Discount discount) {
+        discountedProducts.put(name, discount);
+    }
 
+    private Map<ProductName, List<Item>> groupItemsByDiscountEligibility(List<Item> items) {
+        Set<ProductName> productsWithDiscount = discountedProducts.keySet();
+        return items
+                .stream()
+                .filter(item -> productsWithDiscount.contains(item.name()))
+                .collect(groupingBy(Item::name));
     }
 }
---
+++ src/test/java/kata/supermarket/unit/pricing/SupermarketDiscounterTest.java
@@ -0,0 +1,58 @@
+package kata.supermarket.pricing;
+
+import kata.supermarket.product.Item;
+import kata.supermarket.product.Product;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.ZERO;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static kata.supermarket.product.ProductName.COOKIES;
+import static kata.supermarket.product.ProductName.UNKNOWN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SupermarketDiscounterTest {
+
+    @Test
+    void shouldHandleNoDiscounts() {
+        SupermarketDiscounter supermarketDiscounter = new SupermarketDiscounter();
+        BigDecimal expectedPrice = new BigDecimal("1.00");
+        Item item = new Product(expectedPrice, UNKNOWN).oneOf();
+
+        BigDecimal actualDiscount = supermarketDiscounter.calculateDiscounts(singletonList(item));
+
+        assertEquals(ZERO, actualDiscount);
+    }
+
+    @Test
+    void shouldHandleWhenThereIsADiscountForProduct() {
+        BigDecimal price = new BigDecimal("1.00");
+        SupermarketDiscounter supermarketDiscounter = new SupermarketDiscounter();
+        Discount discountMock = items -> new BigDecimal("0.50");
+        supermarketDiscounter.addDiscount(COOKIES, discountMock);
+        Item item = new Product(price, COOKIES).oneOf();
+
+        BigDecimal actualDiscount = supermarketDiscounter.calculateDiscounts(singletonList(item));
+
+        assertEquals(new BigDecimal("0.50"), actualDiscount);
+    }
+
+    @Test
+    void shouldHandleWhenThereIsMultipleDiscountsAvailable() {
+        BigDecimal price = new BigDecimal("1.00");
+        SupermarketDiscounter supermarketDiscounter = new SupermarketDiscounter();
+        Discount firstDiscount = items -> new BigDecimal("0.50");
+        supermarketDiscounter.addDiscount(COOKIES, firstDiscount);
+        Discount secondDiscount = Items -> new BigDecimal("0.30");
+        supermarketDiscounter.addDiscount(UNKNOWN, secondDiscount);
+
+        Item cookie = new Product(price, COOKIES).oneOf();
+        Item unknownItem = new Product(price, UNKNOWN).oneOf();
+
+        BigDecimal actualDiscount = supermarketDiscounter.calculateDiscounts(asList(cookie, unknownItem));
+
+        assertEquals(new BigDecimal(".80"), actualDiscount);
+    }
+}
