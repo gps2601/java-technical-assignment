923e8a9d2486f21e81465cea8f9363c5dad05dd3
gsmit592 <gsmit592@ford.com>
2022-02-06 13:52:57 +0000

    Removing note in the Basket and also reducing the amount of noise in the tests for Basket

--- src/test/java/kata/supermarket/BasketTest.java
+++ src/test/java/kata/supermarket/BasketTest.java
@@ -6,23 +6,29 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singleton;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BasketTest {
 
-    @DisplayName("basket provides its total value when containing...")
+    @DisplayName("basket provides its total value when containing:")
     @MethodSource
     @ParameterizedTest(name = "{0}")
-    void basketProvidesTotalValue(String description, String expectedTotal, Iterable<Item> items) {
+    void basketProvidesTotalValue(@SuppressWarnings("unused") String description, String expectedTotal, Iterable<Item> items) {
+        BigDecimal expected = new BigDecimal(expectedTotal);
         final Basket basket = new Basket();
         items.forEach(basket::add);
-        assertEquals(new BigDecimal(expectedTotal), basket.total());
+
+        BigDecimal actual = basket.total();
+
+        assertEquals(expected, actual);
     }
 
+    @SuppressWarnings("unused")
     static Stream<Arguments> basketProvidesTotalValue() {
         return Stream.of(
                 noItems(),
@@ -33,50 +39,46 @@ class BasketTest {
         );
     }
 
-    private static Arguments aSingleItemPricedByWeight() {
-        return Arguments.of("a single weighed item", "1.25", Collections.singleton(twoFiftyGramsOfAmericanSweets()));
+    private static Arguments noItems() {
+        return Arguments.of("no items", "0.00", emptyList());
     }
 
-    private static Arguments multipleItemsPricedByWeight() {
-        return Arguments.of("multiple weighed items", "1.85",
-                Arrays.asList(twoFiftyGramsOfAmericanSweets(), twoHundredGramsOfPickAndMix())
+    private static Arguments aSingleItemPricedPerUnit() {
+        return Arguments.of(
+                "a single item priced per unit", "0.49",
+                singleton(unitItem("0.49"))
         );
     }
 
     private static Arguments multipleItemsPricedPerUnit() {
-        return Arguments.of("multiple items priced per unit", "2.04",
-                Arrays.asList(aPackOfDigestives(), aPintOfMilk()));
+        return Arguments.of(
+                "multiple items priced per unit",
+                "2.04",
+                asList(unitItem("1.55"), unitItem("0.49"))
+        );
     }
 
-    private static Arguments aSingleItemPricedPerUnit() {
-        return Arguments.of("a single item priced per unit", "0.49", Collections.singleton(aPintOfMilk()));
+    private static Arguments aSingleItemPricedByWeight() {
+        return Arguments.of(
+                "a single weighed item",
+                "1.25",
+                singleton(weightItem("4.99", ".25"))
+        );
     }
 
-    private static Arguments noItems() {
-        return Arguments.of("no items", "0.00", Collections.emptyList());
+    private static Arguments multipleItemsPricedByWeight() {
+        return Arguments.of(
+                "multiple weighed items",
+                "1.85",
+                asList(weightItem("4.99", ".25"), weightItem("2.99", ".2"))
+        );
     }
 
-    private static Item aPintOfMilk() {
-        return new Product(new BigDecimal("0.49")).oneOf();
+    private static Item unitItem(String price) {
+        return new Product(new BigDecimal(price)).oneOf();
     }
 
-    private static Item aPackOfDigestives() {
-        return new Product(new BigDecimal("1.55")).oneOf();
-    }
-
-    private static WeighedProduct aKiloOfAmericanSweets() {
-        return new WeighedProduct(new BigDecimal("4.99"));
-    }
-
-    private static Item twoFiftyGramsOfAmericanSweets() {
-        return aKiloOfAmericanSweets().weighing(new BigDecimal(".25"));
-    }
-
-    private static WeighedProduct aKiloOfPickAndMix() {
-        return new WeighedProduct(new BigDecimal("2.99"));
-    }
-
-    private static Item twoHundredGramsOfPickAndMix() {
-        return aKiloOfPickAndMix().weighing(new BigDecimal(".2"));
+    private static Item weightItem(String price, String weight) {
+        return new WeighedProduct(new BigDecimal(price)).weighing(new BigDecimal(weight));
     }
 }
